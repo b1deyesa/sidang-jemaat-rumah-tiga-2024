99db7b9bece10137e1dfcc719fb7b14a4474b7ee
= <=>
2024-01-23 17:13:10 +0900

    Absensi & Grafik

--- app/View/Components/layout/Dashboard.php
+++ app/View/Components/layout/Dashboard.php
@@ -12,7 +12,7 @@ class Dashboard extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public $title = ''
+        public $title = 'Dashboard'
     )
     {
         $this->title = $title;
